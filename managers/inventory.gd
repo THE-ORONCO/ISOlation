7599673e2b09e85fc280a9c9f748ad9b07f646cf
Théo Roncoletta <theo.roncoletta@posteo.net>
2025-05-25 16:21:30 +0200

    added item swapping

--- managers/inventory.gd
+++ managers/inventory.gd
@@ -3,16 +3,20 @@ extends Node
 var slot: Item = null
 var player: Node2D = null
 
-func pick_up(item: Item) -> void:
-	if slot != null:
-		slot.remove_from(player)
+func pick_up(new_item: Item) -> void:
+	var old_item: Item = slot
 	
-	slot = item
+	if old_item != null:
+		old_item.remove_from(player)
+		old_item.reparent.call_deferred(player.get_parent())
+		old_item.global_position = new_item.global_position
+	
+	slot = new_item
 	
 	if player != null:
-		item.apply(player)
-		item.reparent.call_deferred(player)
-		item.set_deferred("position", Vector2.ZERO)
+		new_item.apply(player)
+		new_item.reparent.call_deferred(player)
+		new_item.set_deferred("position", Vector2.ZERO)
 
 func use():
 	if slot != null && player != null:
